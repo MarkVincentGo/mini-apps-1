f8bfcf3909d5ba3e515cab20ddb80bbe312b3473
Mark Go <mark.vince.go@gmail.com>
2020-01-14 11:28:09 -0800

    added event listener to submit button and successfully logs textarea to the console

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,8 @@
+// DEFINING FUNCTIONS OF EACH ELEMENT
 
+let textInput = document.getElementById('textInput');
+let submitButton = document.getElementById('submit');
+submitButton.addEventListener('click', (event) => {
+  console.log(textInput.value)
+})
 
