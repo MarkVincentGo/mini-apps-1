ce32675b242d1699a2d3f856bbfea73514841ff7
Mark Go <mark.vince.go@gmail.com>
2020-01-15 12:37:52 -0800

    set up backend. changed states on clicking checkout. set up most components

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,83 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 'home'
+    };
+  }
+
+  //componentDidMount()
+  checkout() {
+    this.setState({
+      currentPage: 'F1'
+    })
+  }
+
+  render() {
+    
+
+    return (
+      <div>
+        <Nav/>
+        {(this.state.currentPage === 'home' ? <HomePage checkout={this.checkout.bind(this)}/> : <FormPage/>)}
+      </div>
+    )
+  }
+};
+
+// NAV BAR =============================================================
+const Nav = (props) => (
+  <h1>MONTE VERDE</h1>
+);
+
+// HOME PAGE ===========================================================
+const HomePage = (props) => (
+  <>
+    <CheckoutButton checkout={props.checkout}/>
+  </>
+);
+
+// CHECKOUT BUTTON =====================================================
+const CheckoutButton = (props) => (
+  <>
+    <button onClick={props.checkout}>Checkout</button>
+    <br/>
+  </>
+);
 
 
+// FORM PAGE ===========================================================
+
+class FormPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      // inputs dynamic according to needs
+      // button with method applied
+      <>
+        <InputBar/>
+        <button>Next</button>
+      </>
+    )
+  }
+}
+
+// INPUT BAR ========================================================
+
+const InputBar = (props) => (
+  // placeholer is passed in as a prop
+  // value will be passed up
+  <input placeholder={props.name}></input>
+)
+
+
+
+
+
+
+
+ReactDOM.render(<App/>, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+const port = 3000;
 
+const app = express();
 
+app.use(express.static(path.join(__dirname, './public')));
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.get('/poop', (req, res) => {
+  res.send('ITS ALIVEEEEEE!')
+})
+
+app.listen(port, console.log(`AYE YO ITS LISTENING ON PORT ${port}`));
